e58af21f6c3d5f7bd34a1f8b3f69e4b01d4e75aa
sgroi-l <74066857+sgroi-l@users.noreply.github.com>
2023-01-25 14:24:14 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+const date = new Date();
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+const caption = document.querySelector('.caption');
+
+/* Generate todays date */
+
+document.querySelector("#date").innerHTML = `Todays date: ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+
+
+/* Declaring the array of image filenames */
+
+const pics = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg', 'pic6.jpg', 'pic7.jpg' ];
+
+
+/* Declaring the alternative text for each image file */
+
+const altText = {
+    pic1: "Nadia looking over the new plot",
+    pic2: "The new plot in all its weedy glory",
+    pic3: "Brambles running wild in a cage",
+    pic4: "Laurie weeding inside a cage",
+    pic5: "Nadia sitting on an oak sleeper after a hard day's work",
+    pic6: "Spring 2022: Bed building completed. Time to get the seedlings in",
+    pic7: "Summer 2022: Flowers and herbs growing happily in a planter"
+  };
+
+/* Looping through images */
+for (i = 0; i < pics.length; i++) {
+  const filename = pics[i];
+  const path = 'images/' + filename;
+  const alt = altText[filename.slice(0, -4)];
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', path);
+  newImage.setAttribute('alt', alt);
+  thumbBar.appendChild(newImage);
+  
+  newImage.addEventListener('click', () => {
+      displayedImage.setAttribute('src', path);
+      displayedImage.setAttribute('alt', alt);
+      caption.textContent = alt;
+  })
+ }
+
